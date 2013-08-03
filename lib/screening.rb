c91f469b8329a7ea66386142b054dc894f164382
phyten <phyten.obr@gmail.com>
2013-08-03 15:12:45 +0900

    {〜}をdo〜endへと変更

--- lib/screening.rb
+++ lib/screening.rb
@@ -8,8 +8,8 @@ module ArrayToSelfConvert
   def self.included(klass)
     methods = ::Array.public_instance_methods(true) - ::Kernel.public_instance_methods(false)
     methods |= ["to_s","to_a","inspect","==","=~","==="]
-    methods.each {|method|
-      define_method(method) {|*args, &block|
+    methods.each do |method|
+      define_method(method) do |*args, &block|
         res = super(*args, &block)
         if res.class == Array && method != 'to_a'
           cloned = deep_clone ? Marshal.load(Marshal.dump(self)) : self.dup
@@ -17,8 +17,8 @@ module ArrayToSelfConvert
         else
           res
         end
-      }
-    }
+      end
+    end
   end
   attr_accessor :deep_clone
 end
