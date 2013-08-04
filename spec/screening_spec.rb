b22dfe7602d088054237cd313d90d5b6e02176db
phyten <phyten.obr@gmail.com>
2013-08-04 20:22:31 +0900

    add several tests

--- spec/screening_spec.rb
+++ spec/screening_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
-# describe MyGem do
-#   it { should be_true }
-# end
+describe Screening do
+  before do    
+    @data = Screening::Data.new    
+    @data.start do |element|
+      element.title        = "fake"
+      element.pageview     = 10000
+    end    
+    @data.start do |element|
+      element.title        = "good"
+      element.pageview     = 10000
+    end    
+    @data.start do |element|
+      element.title        = "good"
+      element.pageview     = 0
+    end    
+    @data.start do |element|
+      element.title        = "soso"
+      element.pageview     = 10000
+    end
+  end
+  describe "Screening::Data" do
+    it "should be Screening::Data" do
+      expect(@data).to be_a_kind_of Screening::Data
+    end
+    describe "Screening::Statistics" do
+      it "should be Screening::Statistics" do        
+        @data.each do |data|
+          expect(data).to be_a_kind_of Screening::Statistics
+        end     
+      end
+      it "should add an element by method 'start'" do
+        @data.start do |element|
+          element.title = "test"
+        end
+        expect(@data).to have(5).screening_statistics
+        new_data = @data.last
+        expect(new_data).to be_a_kind_of Screening::Statistics
+      end
+      # it "should add an element by method default method 'push'" do
+      #   @data.push({title: "adding element"})
+      #   expect(@data).to have(5).screening_statistics
+      #   new_data = @data.last
+      #   expect(new_data).to be_a_kind_of Screening::Statistics
+      # end
+    end
+  end
+end
